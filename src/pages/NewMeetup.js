f8c1e1f76ccfd2a1607f8f939957ee23e08390ce
ekincbc <ekincbc@gmail.com>
2022-12-02 09:24:28 +0100

    created forms

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,11 @@
-import React from "react";
-
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 const NewMeetup = () => {
-  return <div>NewMeetup</div>;
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 };
 
 export default NewMeetup;
