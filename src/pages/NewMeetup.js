9ed6d6c565d32934a9e68e53e496c8f232d48214
ekincbc <ekincbc@gmail.com>
2022-12-03 08:58:04 +0100

    forms has been created and data is getting fetched from api

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 const NewMeetup = () => {
+  const navigate = useNavigate();
+
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+      "https://meetup-data-7a56d-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate.replace("/");
+    });
+  };
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
